3276e74e1fdafd24312ae74694beed848568375f
sumitramteke <sumitvramteke@gmail.com>
2016-09-11 09:42:10 +0530

    deploy on s3 procedure

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
+  host:'http://52.11.130.33:9000'
   namespace: 'api',
   authorizer: 'authorizer:application'
 });
