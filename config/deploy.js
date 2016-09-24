a41624ec2a8c0f6f161d3258221d435f180afa4c
sumitramteke <sumitvramteke@gmail.com>
2016-09-24 17:58:48 +0530

    removing s3 credentials

--- config/deploy.js
+++ config/deploy.js
@@ -8,35 +8,35 @@ module.exports = function(deployTarget) {
     'revision-data': {
       type: 'git-commit'
     },
-    /*'s3-index': {
-      accessKeyId: "AKIAI7US6P75ELDVY7JQ",
-      secretAccessKey: "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",
+    's3-index': {
+      accessKeyId: "<<accessKeyId>>",
+      secretAccessKey: "<<secretAccessKey>>",
       bucket: "workypeui",
       region: "us-west-2",
       allowOverwrite: true
     },
     's3': {
-      accessKeyId: "AKIAI7US6P75ELDVY7JQ",
-      secretAccessKey: "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",
+      accessKeyId: "<<accessKeyId>>",
+      secretAccessKey: "<<secretAccessKey>>",
       bucket: "workypeui",
       region: "us-west-2"
-    }*/
+    }
     // include other plugin configuration that applies to all deploy targets here
-    'ssh-index': {
+    /*'ssh-index': {
       remoteDir: "/usr/local/www/workype",
       username: "ubuntu",
       host: "52.11.130.33",
-      privateKeyFile: "F:\\aws\\workype-app.pem",
+      privateKeyFile: "<<pem-path>>",
       allowOverwrite: true
     },
     rsync: {
       type: 'rsync',
       dest: "/usr/local/www/workype",
       recursive: true,
-      host: "ubuntu@52.11.130.33",
+      host: "user@hostipaddr",
       delete: false,
       args: ['--verbose', "--rsync-path='sudo -u www-data rsync'", '-ztl']
-    }
+    }*/
   };
 
   if (deployTarget === 'development') {
--- config/environment.js
+++ config/environment.js
@@ -52,7 +52,7 @@ module.exports = function(environment) {
     }
   }
 
-  console.log(ENV);
+  // console.log(ENV);
 
   return ENV;
 };
